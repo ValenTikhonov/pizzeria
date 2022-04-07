7844c7a03bfa11724e65c848434153c543b26c23
Valentin_Tikhonov <valentin.tikhonov@capgemini.com>
2022-04-07 13:40:23 +0200

    Creando entidades

---
+++ src/main/java/com/bootcamp/entities/Pizza.java
@@ -0,0 +1,10 @@
+package com.bootcamp.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pizza implements Serializable{ // serializar a un objeto de java
+    
+}
